8ddf0418ea08f5efb7c669ba8a258abd94946b9a
M-Vanessa <vanemaru29@gmail.com>
2022-10-02 19:13:29 -0400

    Pantallas

--- model/navigation.php
+++ model/navigation.php
@@ -5,7 +5,7 @@
                 $module = "view/module/".$links.".html";
             } else if ($links == "blog" || $links == "reserves" || $links == "delivery" || $links == "login") {
                 $module = "view/module/".$links.".php";
-            } else if ($links = "index") {
+            } else if ($links == "index") {
                 $module = "view/module/index.html";
             } else {
                 $module = "view/module/index.html";
--- view/module/delivery.php
+++ view/module/delivery.php
@@ -1,13 +1,61 @@
-<section class="delivery__container w-100 vh-100 py-5">
+<section class="delivery__container py-5">
     <h1 class="text-center text-white my-5"> Solicitar Pedido </h1>
 
-    <section class="container">
+    <!-- sidebar -->
+    <div class="delivery__sidebar">
+        <a href="#" class="delivery__sidebar-link"> Bebidas </a>
+        <a href="#" class="delivery__sidebar-link"> Carnes </a>
+        <a href="#" class="delivery__sidebar-link"> Ensaladas </a>
+        <a href="#" class="delivery__sidebar-link"> Pastas </a>
+        <a href="#" class="delivery__sidebar-link"> Pizzas </a>
+        <a href="#" class="delivery__sidebar-link"> Postres </a>
+        <a href="#" class="delivery__sidebar-link"> Sopas </a>
+    </div>
+
+    <!-- cards -->
+    <section class="container container__card">
         <div class="delivery__card">
-            <img src="view/../public/assets/delivery/1.webp" alt="Delivery ROMANZA" class="delivery__card-image">
+            <img src="view/../public/assets/delivery/drink1.webp" alt="Delivery ROMANZA" class="delivery__card-image">
             <div class="delivery__card-content shadow px-2">
                 <a href="#" class="btn btn-danger delivery__card-view"> Detalles </a>
-                <p class="delivery__card-name"> Nombre del Plato </p>
-                <span class="delivery__card-price"> $00 </span>
+                <p class="delivery__card-name"> Cóctel Americano </p>
+                <span class="delivery__card-price"> $29.50 </span>
+            </div>
+        </div>
+
+        <div class="delivery__card">
+            <img src="view/../public/assets/delivery/drink2.webp" alt="Delivery ROMANZA" class="delivery__card-image">
+            <div class="delivery__card-content shadow px-2">
+                <a href="#" class="btn btn-danger delivery__card-view"> Detalles </a>
+                <p class="delivery__card-name"> Cóctel Caipirinha </p>
+                <span class="delivery__card-price"> $18,90 </span>
+            </div>
+        </div>
+        
+        <div class="delivery__card">
+            <img src="view/../public/assets/delivery/drink3.webp" alt="Delivery ROMANZA" class="delivery__card-image">
+            <div class="delivery__card-content shadow px-2">
+                <a href="#" class="btn btn-danger delivery__card-view"> Detalles </a>
+                <p class="delivery__card-name"> Té de Burbujas </p>
+                <span class="delivery__card-price"> $06,00 </span>
+            </div>
+        </div>
+
+        <div class="delivery__card">
+            <img src="view/../public/assets/delivery/drink4.webp" alt="Delivery ROMANZA" class="delivery__card-image">
+            <div class="delivery__card-content shadow px-2">
+                <a href="#" class="btn btn-danger delivery__card-view"> Detalles </a>
+                <p class="delivery__card-name"> Sorbetes </p>
+                <span class="delivery__card-price"> $14,98 </span>
+            </div>
+        </div>
+
+        <div class="delivery__card">
+            <img src="view/../public/assets/delivery/drink5.webp" alt="Delivery ROMANZA" class="delivery__card-image">
+            <div class="delivery__card-content shadow px-2">
+                <a href="#" class="btn btn-danger delivery__card-view"> Detalles </a>
+                <p class="delivery__card-name"> Jugos Naturales </p>
+                <span class="delivery__card-price"> $03,00 </span>
             </div>
         </div>
     </section>
